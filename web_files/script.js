f8975b6aa65f86bf0dd4a2f40cf6ee2d272b35f9
stilyans <80647243+stilyans@users.noreply.github.com>
2023-05-27 13:22:10 +0300

    Update script.js

--- web_files/script.js
+++ web_files/script.js
@@ -15,3 +15,17 @@ const form = document.querySelector('#contact-form');
 button.addEventListener('click', function() {
   form.classList.toggle('hidden');
 });
+
+// Services button click event
+const servicesButton = document.querySelector('#services-button');
+loginButton.addEventListener('click', function() {
+  // Perform login functionality here
+  console.log("Button selected");
+});
+
+// Industries button click event
+const industriesButton = document.querySelector('#industries-button');
+signupButton.addEventListener('click', function() {
+  // Perform signup functionality here
+  console.log("Button clicked");
+});
